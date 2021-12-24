6e9bab89441e1b0a83733fb7d9ab91c2459d7081
VICSOLWANG <413834325@qq.com>
2021-12-24 16:50:13 +0800

    feat: update eslint config

---
+++ src/config.js
@@ -0,0 +1,64 @@
+/**
+ * Created by VICSOLWANG.
+ * Date: 2021/12/18 20:39
+ * Email: vic.sol.wang@gmail.com
+ */
+
+const getVueConfig = () => {
+  let config = 'plugin:vue/recommended';
+  try {
+    const Vue = require('vue').default;
+    const version = Number(Vue.version.split('.')[0]);
+    if (version === 3) {
+      config = 'plugin:vue/vue3-recommended';
+    }
+  } catch (err) {
+    console.info(
+      'Note: Vue version is not confirmed, use vue2 config by default.',
+    );
+  }
+  return config;
+};
+
+const vueConfig = getVueConfig();
+
+const customRules = {
+  'global-require': 'warn',
+  'import/no-dynamic-require': 'warn',
+  'no-bitwise': 'off',
+  'no-nested-ternary': 'off',
+  'no-script-url': 'warn',
+  'no-underscore-dangle': 'off',
+};
+
+const config = {
+  extends: ['airbnb-base', vueConfig, 'prettier'],
+  parser: 'vue-eslint-parser',
+  plugins: ['vue'],
+  rules: customRules,
+};
+
+try {
+  require('typescript');
+  require('@typescript-eslint/parser');
+  require('@typescript-eslint/eslint-plugin');
+  config.overrides = [
+    {
+      files: ['*.ts', '*.tsx'],
+      extends: ['airbnb-base', 'airbnb-typescript/base', vueConfig, 'prettier'],
+      parser: 'vue-eslint-parser',
+      parserOptions: {
+        parser: '@typescript-eslint/parser',
+        project: './tsconfig.json',
+      },
+      plugins: ['vue'],
+      rules: customRules,
+    },
+  ];
+} catch (err) {
+  console.info(
+    'Note: Typescript eslint needs to install typescript, @typescript-eslint/parser, @typescript-eslint/eslint-plugin.',
+  );
+}
+
+module.exports = config;
